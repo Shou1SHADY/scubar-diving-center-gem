fc8d6dff388ff09caed8b267bfcced50ce6a2b69
Shady Ossama <shadyosama658@gmail.com>
2025-11-30 22:18:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7414847279-ca04f",
+  "appId": "1:758858693404:web:aaef93f974ed7030d658d9",
+  "apiKey": "AIzaSyA4PcWTHZb-oBHvU9a9KajZIXogPCGGvQY",
+  "authDomain": "studio-7414847279-ca04f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "758858693404"
+};
